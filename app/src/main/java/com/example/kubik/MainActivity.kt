715f961f3dec0990b1bc0944bb91f9c38bc5103f
DSaven <55620327+dsaven@users.noreply.github.com>
2019-09-21 13:26:25 +0300

    фдфдфдфдфдфдфдфдфд

---
+++ app/src/main/java/com/example/kubik/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kubik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            genNumber.text = (0..10).random().toString()
+        }
+    }
+}
